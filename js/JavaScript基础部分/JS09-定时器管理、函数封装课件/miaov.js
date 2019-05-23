711993f72ba564345bb190f14805399414de0de4
xuli309 <jswawa@qq.com>
2019-05-23 09:31:05 +0800

    https://github.com/xuli309/-.git

--- js/JavaScript基础部分/JS09-定时器管理、函数封装课件/miaov.js
+++ js/JavaScript基础部分/JS09-定时器管理、函数封装课件/miaov.js
@@ -31,3 +31,15 @@ function doMove ( obj, attr, dir, target, endFn ) {
 }
 
 function getStyle ( obj, attr ) { return obj.currentStyle?obj.currentStyle[attr] : getComputedStyle( obj )[attr]; }
+
+
+
+function $( v ){
+	if( typeof v === 'function' ){
+		window.onload = v;
+	} else if ( typeof v === 'string' ) {
+		return document.getElementById(v);
+	} else if ( typeof v === 'object' ) {
+		return v;
+	}
+}
