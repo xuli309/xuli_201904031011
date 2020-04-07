531501162b986817b670a3ebd500ba763c3876c7
xuli309 <jswawa@qq.com>
2020-04-07 10:07:47 +0800

    https://github.com/xuli309/xuli_201904031011.git

---
+++ vue/vue_smart/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        登录
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'login',
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
