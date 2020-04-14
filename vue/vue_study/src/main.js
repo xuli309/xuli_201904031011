2177c71714b0e0d5d7e72111af95701a7965fc7e
xuli309 <jswawa@qq.com>
2020-04-14 09:33:05 +0800

    https://github.com/xuli309/xuli_201904031011.git

--- vue/vue_study/src/main.js
+++ vue/vue_study/src/main.js
@@ -36,28 +36,28 @@ new Vue({
 //   }
 // }
 
-Vue.prototype.$dispatch = function(eventName,data){
-  let parent = this.$parent;
-  //查找父元素
-  while(parent){
-    //父元素$emit触发
-    parent.$emit(eventName, data);
-    //递归查找父元素
-    parent = parent.$parent;
-  }
-}
+// Vue.prototype.$dispatch = function(eventName,data){
+//   let parent = this.$parent;
+//   //查找父元素
+//   while(parent){
+//     //父元素$emit触发
+//     parent.$emit(eventName, data);
+//     //递归查找父元素
+//     parent = parent.$parent;
+//   }
+// }
 
-Vue.prototype.$boardcast = function(eventName,data){
-  boardcast.call(this,eventName,data);
-};
+// Vue.prototype.$boardcast = function(eventName,data){
+//   boardcast.call(this,eventName,data);
+// };
 
-function boardcast(eventName,data){
-  this.$children.forEach(child => {
-    // 子元素触发$emit
-    child.$emit(eventName,data);
-    if(child.$children.length){
-      // 递归调用，通过call this 指向 child
-      boardcast.call(child,eventName,data);
-    }
-  });
-}
+// function boardcast(eventName,data){
+//   this.$children.forEach(child => {
+//     // 子元素触发$emit
+//     child.$emit(eventName,data);
+//     if(child.$children.length){
+//       // 递归调用，通过call this 指向 child
+//       boardcast.call(child,eventName,data);
+//     }
+//   });
+// }
